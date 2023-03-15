c11bde949528ff328e14e19a317e0b9b66388fe9
Jānis Bērziņš <125191092+zirafjanis@users.noreply.github.com>
2023-03-15 18:51:24 +0200

    final

--- main.py
+++ main.py
@@ -2,25 +2,25 @@
 
 def parallel_processing(n, m, data):
     output = []
-    threads = [0] * n # initialize each thread to start at time 0
+    threads = [0] * n 
     for i in range(m):
-        thread_id = threads.index(min(threads)) # find the next available thread
-        start_time = threads[thread_id] # the start time for the task is the current time for the thread
-        output.append((thread_id, start_time)) # add the output pair to the list
-        threads[thread_id] += data[i] # update the current time for the thread based on the processing time for the task
+        thread_id = threads.index(min(threads)) 
+        start_time = threads[thread_id] 
+        output.append((thread_id, start_time)) 
+        threads[thread_id] += data[i] 
     return output
 
 
 
 def main():
-    # read input from keyboard
+    # read input
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
 
-    # simulate parallel processing
+    
     result = parallel_processing(n, m, data)
 
-    # print output pairs
+    # print output
     for thread_id, start_time in result:
         print(thread_id, start_time)
 
