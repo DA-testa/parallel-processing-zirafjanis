2f9a4cdc030d92caa3807a51bdfc3fee4e53124b
Jānis Bērziņš <125191092+zirafjanis@users.noreply.github.com>
2023-03-15 18:46:17 +0200

    test3

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-import heapq
+# python3
 
 def parallel_processing(n, m, data):
     output = []
-    # Create a priority queue and populate it with the first n jobs
-    queue = [(0, i) for i in range(n)]
-    heapq.heapify(queue)
-    # Iterate through the remaining jobs
-    for i in range(n, m):
-        # Get the next available thread and the time it will finish its current job
-        time, thread = heapq.heappop(queue)
-        # Append the thread and the job index to the output
-        output.append((thread, time))
-        # Update the finish time of the thread with the new job
-        finish_time = time + data[i]
-        # Add the thread and its new finish time to the priority queue
-        heapq.heappush(queue, (finish_time, thread))
-    # Process the remaining jobs
-    while queue:
-        time, thread = heapq.heappop(queue)
-        # Append the thread and the job index to the output
-        output.append((thread, time))
-    # Sort the output by the job index
-    output.sort(key=lambda x: x[0])
-    # Format the output as a string
-    output_str = "\n".join([f"{t[0]} {t[1]}" for t in output])
-    return output_str
+    threads = [0] * n # initialize each thread to start at time 0
+    for i in range(m):
+        thread_id = threads.index(min(threads)) # find the next available thread
+        start_time = threads[thread_id] # the start time for the task is the current time for the thread
+        output.append((thread_id, start_time)) # add the output pair to the list
+        threads[thread_id] += data[i] # update the current time for the thread based on the processing time for the task
+    return output
 
 
-# Test
-input_data = "2 5\n1 2 3 4 5"
-expected_output = "0 0\n1 0\n0 1\n1 2\n0 4"
-assert parallel_processing(*map(str.strip, input_data.split('\n'))) == expected_output
+
+def main():
+    # read input from keyboard
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
+
+    # simulate parallel processing
+    result = parallel_processing(n, m, data)
+
+    # print output pairs
+    for thread_id, start_time in result:
+        print(thread_id, start_time)
+
+
+
+if __name__ == "__main__":
+    main()
